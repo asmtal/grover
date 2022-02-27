5789fe2a866d17787a00d612e8c9f64cce684c9c
nimda <nimda@Yuuka>
2022-02-27 16:54:37 +0100

    clean variables files prd & stg

--- live/stg/00_variables.tf
+++ live/stg/00_variables.tf
@@ -8,22 +8,6 @@ variable "region" {
   description = "region où vont etre deployé les ressources aws [eu-west-3 par exemple]"
 }
 
-
-
-# variable "ssh_key" {
-#   type        = string
-#   description = "ssh key a utiliser pour les instances"
-# }
-
-#VPC
-
-#Possible de faire une liste pour les subnets et les zones
-#  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-#  azs      = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-#VPC
-
 variable "vpc_cidr" {
   type        = string
   description = "Plage d'adressage pour le VPC"
